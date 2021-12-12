82961722d68f4ec055eec81293d48bb63269779a
Cesar4280 <cesarjavierfajardoortiz@gmail.com>
2021-12-12 02:22:22 -0500

    Se agrega pequeña implementacion de middleware

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,9 @@ const { getOneById } = require("../middlewares/handlerError");
 const { userController: controller } = require("../controllers");
 
 router.get("/", controller.getUsers);   // todos los usuarios
-router.get("/:id", controller.getUser, getOneById); // un usuario por id
+router.get("/:id", controller.getUser); // un usuario por id
 router.post("/", controller.addUser);   // agregar un usuarios
+
+router.use(getOneById);
 
 module.exports = router;
