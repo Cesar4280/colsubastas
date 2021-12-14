69bf46b4a17d38cf876051bc8f7dfc8ae97d2db4
Cesar4280 <cesarjavierfajardoortiz@gmail.com>
2021-12-14 03:12:49 -0500

    comentario en usuario

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,6 @@ const { userController: controller } = require("../controllers");
 
 router.get("/", controller.getUsers);   // todos los usuarios
 router.get("/:id", controller.getUser); // un usuario por id
-router.post("/", controller.addUser);   // agregar un usuarios
+router.post("/", controller.addUser);   // agregar un usuario
 
 module.exports = router;
