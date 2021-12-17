ac28b4f9fdb99afbf775c7e0fc0111d26e9021d2
Cesar4280 <cesarjavierfajardoortiz@gmail.com>
2021-12-16 21:41:02 -0500

    CRUD Products pre final

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,13 @@
 const userRoutes = require("./users");
 const productRoutes = require("./products");
 
-/*
-const saleRoutes = require("./sales");
-const dressRoutes = require("./dresses");
-*/
+// const saleRoutes = require("./sales");
 
 const router = require("express").Router();
 
 router.use("/users", userRoutes);
 router.use("/products", productRoutes);
 
-/*
-router.use("/sales", saleRoutes);
-router.use("/dresses", dressRoutes);
-*/
+// router.use("/sales", saleRoutes);
 
 module.exports = router;
